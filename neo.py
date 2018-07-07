7d58568c4e6de48ac6455934d8a1a4d220567ed3
legion <test@test.com>
2018-07-07 15:52:14 +0530

    001 - First prototype

---
+++ neo.py
@@ -0,0 +1,17 @@
+import aiml 
+import time
+neo = aiml.Kernel()
+neo.learn('intro.aiml')
+# neo.respond("load aiml b")
+
+while True:
+	query = raw_input("YOU :> ")
+	if query.lower() == "stop":
+		time.sleep(0.5)
+		print('Ok Bye')
+		time.sleep(1)
+		print('Nice to chat You :) ')
+		break
+	else:	
+		responce = neo.respond(query)
+		print("Neo:> {}".format(responce))		

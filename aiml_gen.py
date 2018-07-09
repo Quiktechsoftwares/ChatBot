6b746663b1831b9e21aecb6c4df6622106d611dc
legion <test@test.com>
2018-07-09 21:35:35 +0530

    Added Simple AIML Generator python file

---
+++ aiml_gen.py
@@ -0,0 +1,25 @@
+print("""
+|~|||\/||   |~_[~|\ ||~||~)|~|~|~/~\|~)
+|~|||  ||_  |_|[_| \||~||~\|~| | \_/|~\
+	""")
+print("""
+This Python file generates basic AIML file struture between <catogory> tags only
+ In Default it creates normal text, You need to put some extra stuff tags for a fully
+functional
+                 ###### STILL UNDER CONSTRUCTION #########
+                 Enter "stop" to quit
+	""")
+
+filename = raw_input("Enter The File Name: ")
+myfile = open(filename, 'a+')
+while True:
+	query = raw_input("Question: ")
+	if query == "stop":
+		break
+		myfile.close()
+	responce = raw_input("responce: ")
+	myfile.write('<category><pattern>{}</pattern>'.format(query))
+	myfile.write('<template>{}</template></category>'.format(responce))
+	myfile.write("\n")
+	print("\n")
+	
